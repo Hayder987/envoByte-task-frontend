3298a05c0a39753075f670ed2e5696c7e3fb5122
hayder <hayderbd4290@gmail.com>
2025-03-19 22:44:17 +0600

    create navbar sticky

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -9,74 +9,81 @@ const NavBar = () => {
   const [menu, setMenu] = useState(false);
 
   return (
-    <nav
-      className={`${
-        pathname === "/" ? "bg-[#03135b]" : "bg-blue-700"
-      } flex py-3 px-4 lg:px-12 justify-between relative items-center`}
-    >
-      {/* logo */}
-      <img src={logo} alt="" className="" />
-      {/* menu */}
-      <div className="">
-        <ul className="uppercase hidden textWhite lg:flex justify-center items-center gap-6">
-          <NavLink to="/">
-            <li className="">Home</li>
-          </NavLink>
-          <NavLink to="/services">
-            <li className="">Service</li>
-          </NavLink>
-          <NavLink to="/combosales">
-            <li className="">combo sales</li>
-          </NavLink>
-          <NavLink to="/portfolio">
-            <li className="">portfolio</li>
-          </NavLink>
-          <NavLink to="/aboutUs">
-            <li className="">about us</li>
-          </NavLink>
-          <NavLink to="/blogs">
-            <li className="">blogs</li>
-          </NavLink>
-        </ul>
-      </div>
-      {/* side-menu */}
-      <div className={`absolute duration-300 top-26 ${menu?'left-0':'-left-[1800px]'} flex p-6 lg:hidden bg-blue-900 w-2/4 h-[100vh] z-10 `}>
-        <ul 
-        onClick={() => setMenu(!menu)}
-        className="uppercase  textWhite flex flex-col gap-4">
-          <NavLink to="/">
-            <li className="">Home</li>
-          </NavLink>
-          <NavLink to="/services">
-            <li className="">Service</li>
-          </NavLink>
-          <NavLink to="/combosales">
-            <li className="">combo sales</li>
-          </NavLink>
-          <NavLink to="/portfolio">
-            <li className="">portfolio</li>
-          </NavLink>
-          <NavLink to="/aboutUs">
-            <li className="">about us</li>
-          </NavLink>
-          <NavLink to="/blogs">
-            <li className="">blogs</li>
-          </NavLink>
-        </ul>
-      </div>
-      {/* menu */}
-      <div onClick={() => setMenu(!menu)} className="flex lg:hidden">
-        <button className="text-white text-2xl cursor-pointer">
-          {menu ? <RxCross2 /> : <RiMenuFold2Line />}
-        </button>
-      </div>
-      {/* button */}
-      <div className="">
-        <button className="btnBg text-sm rounded-sm cursor-pointer md:py-3 py-2 px-8 md:px-14">
-          See Pricing
-        </button>
-      </div>
-    </nav>
+    <div className="sticky z-20 top-0">
+      <nav
+        className={`${
+          pathname === "/" ? "bg-[#03135b]" : "bg-blue-700"
+        } flex py-3 px-4 lg:px-12 justify-between relative items-center`}
+      >
+        {/* logo */}
+        <img src={logo} alt="" className="" />
+        {/* menu */}
+        <div className="">
+          <ul className="uppercase hidden textWhite lg:flex justify-center items-center gap-6">
+            <NavLink to="/">
+              <li className="">Home</li>
+            </NavLink>
+            <NavLink to="/services">
+              <li className="">Service</li>
+            </NavLink>
+            <NavLink to="/combosales">
+              <li className="">combo sales</li>
+            </NavLink>
+            <NavLink to="/portfolio">
+              <li className="">portfolio</li>
+            </NavLink>
+            <NavLink to="/aboutUs">
+              <li className="">about us</li>
+            </NavLink>
+            <NavLink to="/blogs">
+              <li className="">blogs</li>
+            </NavLink>
+          </ul>
+        </div>
+        {/* side-menu */}
+        <div
+          className={`absolute duration-300 top-20 ${
+            menu ? "left-0" : "-left-[1800px]"
+          } flex p-6 lg:hidden bg-blue-900 w-2/4 h-[100vh] z-10 `}
+        >
+          <ul
+            onClick={() => setMenu(!menu)}
+            className="uppercase  textWhite flex flex-col gap-4"
+          >
+            <NavLink to="/">
+              <li className="">Home</li>
+            </NavLink>
+            <NavLink to="/services">
+              <li className="">Service</li>
+            </NavLink>
+            <NavLink to="/combosales">
+              <li className="">combo sales</li>
+            </NavLink>
+            <NavLink to="/portfolio">
+              <li className="">portfolio</li>
+            </NavLink>
+            <NavLink to="/aboutUs">
+              <li className="">about us</li>
+            </NavLink>
+            <NavLink to="/blogs">
+              <li className="">blogs</li>
+            </NavLink>
+          </ul>
+        </div>
+        {/* menu */}
+        <div onClick={() => setMenu(!menu)} className="flex lg:hidden">
+          <button className="text-white text-2xl cursor-pointer">
+            {menu ? <RxCross2 /> : <RiMenuFold2Line />}
+          </button>
+        </div>
+        {/* button */}
+        <div className="">
+          <button className="btnBg text-sm rounded-sm cursor-pointer md:py-3 py-2 px-8 md:px-14">
+            See Pricing
+          </button>
+        </div>
+      </nav>
+    </div>
   );
 };
 
